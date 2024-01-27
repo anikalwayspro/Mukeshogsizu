7fd826538c9a04b6daf7904ab84ef0ab14a95fc4
Anik <135734362+anikalwayspro@users.noreply.github.com>
2024-01-27 11:17:53 +0530

    Update translator.py

--- MukeshRobot/modules/translator.py
+++ MukeshRobot/modules/translator.py
@@ -78,7 +78,7 @@ from telethon import __version__ as s
 from MukeshRobot import OWNER_ID, dispatcher
 from MukeshRobot import pbot as client
 
-Mukesh = "https://te.legra.ph/file/1a72f3770dcb90ee8b3f7.jpg"
+Mukesh = "https://te.legra.ph/file/a52d8e2b457979b7f2b9e.jpg"
 
 
 @client.on_message(filters.command(["repo", "source"]))
@@ -93,7 +93,7 @@ async def repo(client, message):
 **» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
 **» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{z}`
 
-**ɢʀᴏᴜᴘ ✘ ᴄᴏɴᴛʀᴏʟʟᴇʀ sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ ᴩᴜʙʟɪᴄ ᴀɴᴅ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ.**
+**Sizuka ✘ Music sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ private.**
 """,
         reply_markup=InlineKeyboardMarkup(
             [
@@ -103,7 +103,7 @@ async def repo(client, message):
                     ),
                     InlineKeyboardButton(
                         "• ʀᴇᴘᴏ •",
-                        url="\x68\x74\x74\x70\x73\x3A\x2F\x2F\x67\x69\x74\x68\x75\x62\x2E\x63\x6F\x6D\x2F\x4E\x6F\x6F\x62\x2D\x4D\x75\x6B\x65\x73\x68\x2F\x4D\x75\x6B\x65\x73\x68\x52\x6F\x62\x6F\x74",
+                        url="https://t.me/catchme_here/16",
                     ),
                 ]
             ]
